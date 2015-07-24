058c5d17c8eef606d8562e085b45c8d15df9387a
rushfire <functionjunkies@gmail.com>
2015-07-23 22:58:06 -0400

    Added voronoi library. Added polygen js file for generating polygon meshes. Changed index to start phaser

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>World Gen</title>
+        <script src="js/phaser.js"></script>
+        <script src="js/rhill-voronoi-core.js"></script>
+    </head>
+    <body>
+      <script src="js/main.js"></script>
+    </body>
+</html>
